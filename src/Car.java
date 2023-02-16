f61f2217760a21f97ce172784405fb649e5399bb
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:52:46 -0500

    add cars toString and print

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,27 @@
 public class Car {
 
-    private final boolean wheels;
-    private final int cylinders;
+    private boolean wheels;
+    private int cylinders;
 
     public Car(boolean wheels, int cylinders) {
         this.wheels = wheels;
         this.cylinders = cylinders;
+    }       retu    }
+
+    public void setWheels(boolean wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    @Override
+    public String toString() {
+        return "Wheels: " + wheels + " cylinders: " + cylinders + "\n";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
             //FOR INUTIL
             System.out.println(i);
         }
+<<<<<<< HEAD
         System.out.println("Hello world!");
+=======
+        System.out.println(cars);
+>>>>>>> 86ffe66 (add cars toString and print)
     }
 }
